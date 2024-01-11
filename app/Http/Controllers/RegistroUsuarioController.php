b9b04e14bca192ea1b41659acb3541843ac7e3b0
Herick Moreira <123032507+Herick2D@users.noreply.github.com>
2024-01-11 17:12:47 -0300

    fix: controller

--- app/Http/Controllers/RegistroUsuarioController.php
+++ app/Http/Controllers/RegistroUsuarioController.php
@@ -2,9 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\RegistroUsuario;
 use Illuminate\Http\Request;
 
 class RegistroUsuarioController extends Controller
 {
-    //
+    public function index()
+    {
+        $usuarios = RegistroUsuario::all();
+        return response()->json($usuarios);
+    }
+
+    public function show($id)
+    {
+        $usuario = RegistroUsuario::findOrFail($id);
+        return response()->json($usuario);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|unique:registro_usuario,email',
+            'senha' => 'required|min:6',
+        ]);
+
+        $usuario = RegistroUsuario::create($request->all());
+        return response()->json($usuario, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'email' => 'required|email|unique:registro_usuario,email,' . $id,
+            'senha' => 'required|min:6',
+        ]);
+
+        $usuario = RegistroUsuario::findOrFail($id);
+        $usuario->update($request->all());
+
+        return response()->json($usuario, 200);
+    }
+
+    public function destroy($id)
+    {
+        $usuario = RegistroUsuario::findOrFail($id);
+        $usuario->delete();
+
+        return response()->json(null, 204);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PessoaController;
 use App\Http\Controllers\VeiculoController;
 use App\Http\Controllers\RevisaoController;
+use App\Http\Controllers\RegistroUsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,6 +24,12 @@ Route::get('/revisao/{id}', [RevisaoController::class, 'show']);
 Route::post('/revisao', [RevisaoController::class, 'store']);
 Route::put('/revisao/{id}', [RevisaoController::class, 'update']);
 Route::delete('/revisao/{id}', [RevisaoController::class, 'destroy']);
+
+Route::get('/registro-usuario', [RegistroUsuarioController::class, 'index']);
+Route::get('/registro-usuario/{id}', [RegistroUsuarioController::class, 'show']);
+Route::post('/registro-usuario', [RegistroUsuarioController::class, 'store']);
+Route::put('/registro-usuario/{id}', [RegistroUsuarioController::class, 'update']);
+Route::delete('/registro-usuario/{id}', [RegistroUsuarioController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
